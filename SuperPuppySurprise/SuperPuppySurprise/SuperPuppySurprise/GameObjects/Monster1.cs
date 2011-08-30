387d2752e6f874770913921ac6b2d8a4d209133d
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-08-30 00:09:15 +0000

    Added the base of the project Created a basic game object set up Started the Physics Engine

---
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/GameObjects/Monster1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SuperPuppySurprise.GameObjects
+{
+    public class Monster1: GameObject
+    {
+        public Monster1(Vector2 Position) : base(Position)
+        {
+        }
+    }
+}
