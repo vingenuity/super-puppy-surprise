9cf7406f93ab2b53a827f0769633d7308f577e79
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-01 13:39:35 +0000

    Vince broke the last build by not adding PlayerInfo.cs to the svn. I made modifications so it would run.

--- trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/GameObjects/Player.cs
+++ trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/GameObjects/Player.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using SuperPuppySurprise.AIRoutines;
+//using SuperPuppySurprise.AIRoutines;
 
 namespace SuperPuppySurprise.GameObjects
 {
