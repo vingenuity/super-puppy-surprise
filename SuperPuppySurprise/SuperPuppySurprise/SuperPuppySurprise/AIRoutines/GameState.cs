0e1a52cbca23eac1ef976076c87b2d4e1376ed1f
howlingblue@gmail.com <howlingblue@gmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-02 21:19:06 +0000

    Added the necessary AI files that I forgot yesterday. Let me know if I forgot something else.

---
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/AIRoutines/GameState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SuperPuppySurprise.GameObjects;
+
+namespace SuperPuppySurprise.AIRoutines
+{
+    public class GameState
+    {
+        public static Player[] players;
+
+        public GameState()
+        {
+            players = new Player[4];
+        }
+
+        public static Vector2 findClosestPlayer()
+        {
+            return players[0].getPosition();
+        }
+    }
+}
