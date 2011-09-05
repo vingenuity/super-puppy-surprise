70f5b8172bfdabc50a6ea8c5ec22f65979ed040e
mthomasleary@gmail.com <mthomasleary@gmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-05 15:41:47 +0000

    We have bullets! Finally! Fire with space for first player. For reals this time.

---
+++ trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/GameObjects/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SuperPuppySurprise.GameObjects
+{
+    public class Bullet : GameObject
+    {
+        KeyboardState thisKeyState;
+        Texture2D texture;
+        SpriteBatch spriteBatch;
+
+        public Bullet(Vector2 Position, Vector2 Dir)
+            : base(Position) 
+        {
+            Direction = Dir;
+            Speed = 500;
+            Size = new Vector2(4, 4);
+            Radius = 2;
+            Game1.PhysicsEngine.AddTrigger(this);
+            Velocity = Direction * Speed;
+        }
+
+        //at the moment, a bullet looks like a player or monster
+        public override void Load(ContentManager Content, SpriteBatch spriteBatch)
+        {
+            texture = Content.Load<Texture2D>("TestPicture2");
+            this.spriteBatch = spriteBatch;
+            base.Load(Content, spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //detect collision, unload if needed
+        }
+        
+        public override void Draw(GameTime gameTime)
+        {
+            Rectangle r = new Rectangle((int)(Position.X - Size.X / 2), (int)(Position.Y - Size.Y / 2), (int)Size.X, (int)Size.Y);
+            spriteBatch.Draw(texture, r, Color.White);
+            base.Draw(gameTime);
+        }
+    }
+}
