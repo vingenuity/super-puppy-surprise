6ce0ed4d4669b10051cee5fd32bd593dfc067b3e
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-08 14:44:35 +0000

    Particle systems working in game. Try moving the player around. Added background image Make screen smaller

---
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/DPSFParticles/TestParticle2.cs
@@ -0,0 +1,36 @@
+﻿using DPSF.ParticleSystems;
+using System;
+using Microsoft.Xna.Framework;
+using SuperPuppySurprise.GameObjects;
+
+namespace SuperPuppySurprise.DPSFParticles
+{
+    public class TestParticle2 : Particle
+    {
+        Random2DParticleSystem mcSphereParticleSystem;
+        public int counter = 0;
+        public Vector3 Position;
+        GameObject gameObject;
+       
+        public TestParticle2(GameObject gameObject)
+            : base()
+        {
+            this.gameObject = gameObject;
+            mcSphereParticleSystem = new Random2DParticleSystem(Game1.game);
+            mcSphereParticleSystem.AutoInitialize(Game1.game.GraphicsDevice, Game1.game.Content, Game1.game.spriteBatch);
+            
+            ParticleSystem = mcSphereParticleSystem;
+
+            //mcSphereParticleSystem.ChangeSphereRadius(.000000005f);
+        }
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+           
+            
+            mcSphereParticleSystem.Pos = ParticleManager.To3D(gameObject.Position);
+           
+            //mcSphereParticleSystem.Emitter.PositionData.Position = Position;
+            base.Update(gameTime);
+        }
+    }
+}
