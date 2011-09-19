84477fa465b470172e8eabed77a4145e7261e4ae
howlingblue@gmail.com <howlingblue@gmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-19 15:26:05 +0000

    Updated spawner with full spawning capability. Try the new spawns!

--- SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/Spawner.cs
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/Spawner.cs
@@ -13,12 +13,12 @@ namespace SuperPuppySurprise
     {
         static bool areSpawning = false;
         static int lastDoor = 0, lastType = 0;
-        static Int32 spawn_delay = 500;
+        static Int32 spawn_delay = 300;
         static Random rand = new Random();
         static AutoResetEvent autoEvent = new AutoResetEvent(false);
 
-        static private Vector2[] doors = new Vector2[4] {new Vector2(50, 250), new Vector2(250, 50), 
-                                                  new Vector2(450, 250), new Vector2(250, 450)};
+        static private Vector2[] doors = new Vector2[4] {new Vector2(50, 250), new Vector2(450, 250), 
+                                                         new Vector2(250, 50), new Vector2(250, 450)};
 
         public Spawner() { }
 
@@ -47,63 +47,83 @@ namespace SuperPuppySurprise
 
         public void randSpawn()
         {
-            setSpawn(rand.Next(0, 4), 0);
-        }
-
-        public void spawn(int door, int type)
-        {
-            switch (type)
+            if (!areSpawning)
             {
-                case 0:
-                    Thread.Sleep(5000);
-                    makeEnemy('r', door, 0, 0);
-                    Thread.Sleep(spawn_delay);
-                    makeEnemy('r', door, -10, 0);
-                    makeEnemy('r', door, 10, 0);
-                    Thread.Sleep(spawn_delay);
-                    makeEnemy('r', door, 0, 0);
-                    break;
-                case 1:
-                    break;
-                default:
-                    break;
+                setSpawn(rand.Next(0, 2));
             }
         }
-        public void setSpawn(int door, int type)
+        public void setSpawn(int type)
         {
-            lastDoor = door;
             lastType = type;
             if (!areSpawning)
             {
                 ThreadPool.QueueUserWorkItem(new WaitCallback(spawn), autoEvent);
             }
-
-            //Thread sThread = new Thread(spawn);
-            //sThread.Start();
         }
 
         public static void spawn(object stateInfo)
         {
             areSpawning = true;
-            Thread.Sleep(3000);
+            Thread.Sleep(2000);
             switch (lastType)
             {
                 case 0:
-
-                    spawnRunner(0, 0);
-                    spawnRunner(10, 0);
-                    spawnRunner(0, 0);
-                    /*
-                    Runner r2 = new Runner(doors[lastDoor]);
-                    Game1.sceneObjects.Add(r2);
-                    r2.Load(Game1.game.Content, Game1.spriteBatch);
+                    lastDoor = 2;
+                    spawnEnemy('r', 0, 0);
                     Thread.Sleep(spawn_delay);
-                    Runner r3 = new Runner(doors[lastDoor]);
-                    Game1.sceneObjects.Add(r3);
-                    r3.Load(Game1.game.Content, Game1.spriteBatch);
-                    */
+                    spawnEnemy('r', 20, 0);
+                    spawnEnemy('r', -20, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 15, 0);
+                    spawnEnemy('r', -15, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 30, 0);
+                    spawnEnemy('r', -30, 0);
+
+                    lastDoor = 3;
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 20, 0);
+                    spawnEnemy('r', -20, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 15, 0);
+                    spawnEnemy('r', -15, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 30, 0);
+                    spawnEnemy('r', -30, 0);
                     break;
                 case 1:
+                    lastDoor = 0;
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 20, 0);
+                    spawnEnemy('r', -20, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 15, 0);
+                    spawnEnemy('r', -15, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 30, 0);
+                    spawnEnemy('r', -30, 0);
+
+                    lastDoor = 1;
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 20, 0);
+                    spawnEnemy('r', -20, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 0, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 15, 0);
+                    spawnEnemy('r', -15, 0);
+                    Thread.Sleep(spawn_delay);
+                    spawnEnemy('r', 30, 0);
+                    spawnEnemy('r', -30, 0);
                     break;
                 default:
                     break;
@@ -112,22 +132,48 @@ namespace SuperPuppySurprise
             ((AutoResetEvent)stateInfo).Set();
 
         }
-        public static void spawnRunner(int leftOff, int rightOff)
+
+        public static void spawnEnemy(char eType, int lrOff, int udOff)
         {
-            Runner r = new Runner(doors[lastDoor]);
+            Vector2 pos = doors[lastDoor];
+            if (lastDoor < 2) //We are spawning on left or right doors
+            {
+                pos.X += udOff;
+                pos.Y += lrOff;
+            }
+            else
+            {
+                pos.X += lrOff;
+                pos.Y += udOff;
+            }
+
             try
             {
-
-                while (Game1.PhysicsEngine.CollidesWithAnotherObject(r))
+                switch (eType)
                 {
-                    Thread.Sleep(100);
+                    case 'r':
+                        Runner r = new Runner(pos);
+                        while (Game1.PhysicsEngine.CollidesWithAnotherObject(r))
+                        {
+                            Thread.Sleep(100);
+                        }
+                        r.AddGameObjectToScene();
+                        r.Load(Game1.game.Content, Game1.spriteBatch);
+                        break;
+                    case 's':
+                        Shooter s = new Shooter(pos);
+                        while (Game1.PhysicsEngine.CollidesWithAnotherObject(s))
+                        {
+                            Thread.Sleep(100);
+                        }
+                        s.AddGameObjectToScene();
+                        s.Load(Game1.game.Content, Game1.spriteBatch);
+                        break;
+                    default:
+                        break;
                 }
-                r.AddGameObjectToScene();
-                r.Load(Game1.game.Content, Game1.spriteBatch);
-
-                Thread.Sleep(spawn_delay);
             }
-            catch{}
+            catch { }
         }
     }
 }
