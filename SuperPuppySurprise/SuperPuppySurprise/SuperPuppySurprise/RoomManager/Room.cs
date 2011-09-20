353654778fa79c7f11a9746df4f0908905e96fda
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-20 15:59:27 +0000

    Started the code for changing rooms.

---
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/RoomManager/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SuperPuppySurprise.RoomManager
+{
+    public class Room
+    {
+        public Room()
+        {
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/RoomManager/RoomEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SuperPuppySurprise.RoomManager
+{
+    public class RoomEngine
+    {
+        Room room;
+        public RoomEngine()
+        {
+            room = new Level1();
+        }
+        public void Update(GameTime gameTime)
+        {
+            room.Update(gameTime);
+        }
+    }
+}
