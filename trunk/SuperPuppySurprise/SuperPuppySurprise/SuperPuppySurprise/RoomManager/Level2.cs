cc191cc0539150cca1bad6190012e68164daf0bb
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-22 03:10:20 +0000

    Made it more game like

--- trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/RoomManager/Level2.cs
+++ trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/RoomManager/Level2.cs
@@ -5,6 +5,7 @@ using System.Text;
 using SuperPuppySurprise.AIRoutines;
 using SuperPuppySurprise.Spawning;
 using SuperPuppySurprise.GameMech;
+using SuperPuppySurprise.GameObjects;
 
 namespace SuperPuppySurprise.RoomManager
 {
@@ -29,15 +30,39 @@ namespace SuperPuppySurprise.RoomManager
         }
         public void CheckIfProcede()
         {
+            Game1.hud.ShowLevelClearedText = false;
             if (GameMechanics.Score > 2000)
             {
-                Game1.RoomManager.ChangeRoom(new Level3());
+                StillSpawning = false;
+                if (GameState.enemies.Count == 0 && Game1.SpawnManager.SpawnList.Count == 0)
+                {
+                    Game1.hud.ShowLevelClearedText = true;
+                    if (!NextLevel)
+                    {
+                        NextLevel = true;
+                        new EnterNextRoomTrigger(RoomType.Level2);
+                        for (int i = 0; i < GameState.players.Count; i++)
+                            GameState.players[i].GoToRoom(400, 250);
+                    }
+                }
             }
+            
         }
+        bool StillSpawning = true;
+        bool NextLevel = false;
+        double preloadingTime = 5;
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            CheckIfProcede();
-            SetSpawn();
+            if (preloadingTime > 0)
+            {
+                preloadingTime -= gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                CheckIfProcede();
+                if (StillSpawning)
+                    SetSpawn();
+            }
             base.Update(gameTime);
         }
 
