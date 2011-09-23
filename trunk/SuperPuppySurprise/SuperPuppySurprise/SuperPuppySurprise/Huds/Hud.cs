056cfdd609c5eff6c046f834acd1d9bce1d06ce4
motasheron@hotmail.com <motasheron@hotmail.com@d756bf54-94a6-73af-0e43-e25e517bd726>
2011-09-23 12:10:26 +0000

    added bullet ammo images code

--- trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/Huds/Hud.cs
+++ trunk/SuperPuppySurprise/SuperPuppySurprise/SuperPuppySurprise/Huds/Hud.cs
@@ -18,6 +18,7 @@ namespace SuperPuppySurprise.Huds
         Texture2D LightOff;
         Texture2D complete;
         Texture2D sidebar;
+        Texture2D gun1, gun2, gun3;
         RoomMap roomMap;
         Rectangle rectFinish = new Rectangle(100, 100, 300, 300);
         public Hud()
@@ -33,6 +34,9 @@ namespace SuperPuppySurprise.Huds
             LightOff = Game1.game.Content.Load<Texture2D>("LightOn");
             complete = Game1.game.Content.Load<Texture2D>("levelcomplete");
             sidebar = Game1.game.Content.Load<Texture2D>("asset_sideBoard");
+            gun1 = Game1.game.Content.Load<Texture2D>("asset_icon_tripleFire");
+            gun2 = Game1.game.Content.Load<Texture2D>("asset_icon_spread");
+            gun3 = Game1.game.Content.Load<Texture2D>("asset_icon_minigun");
         }
         public void Update(GameTime gameTime)
         {
@@ -47,9 +51,42 @@ namespace SuperPuppySurprise.Huds
             {
                 p = (Player)GameState.players[0];
                 spriteBatch.DrawString(spriteFont, "" + GameMechanics.Score, new Vector2(620, 60), Color.White);
-                spriteBatch.DrawString(spriteFont, "Super Shotty: " + p.rounds[2], new Vector2(600, 85), Color.White);
-                spriteBatch.DrawString(spriteFont, "Burstfire: " + p.rounds[3], new Vector2(600, 110), Color.White);
-                spriteBatch.DrawString(spriteFont, "Automatic: " + p.rounds[4], new Vector2(600, 135), Color.White);
+                int rounds = p.rounds[2];
+                int offsetY = 25;
+                int offsetX = 25;
+                int i = 0; int j = 0;
+                while (rounds > 25 && i<4)
+                {
+                    spriteBatch.Draw(gun1, new Rectangle(614 + offsetX * i, 85 + offsetY * j, 15, 15), Color.White);
+                    i++;
+                    rounds -= 25;
+                    //spriteBatch.DrawString(spriteFont, "Super Shotty: " + p.rounds[2], new Vector2(600, 85), Color.White);
+                }
+                j++;
+                i = 0;
+                rounds = p.rounds[3];
+                while (rounds > 25 && i < 4)
+                {
+                    spriteBatch.Draw(gun2, new Rectangle(614 + offsetX * i, 85 + offsetY * j, 15, 15), Color.White);
+                    i++;
+                    rounds -= 25;
+                   // spriteBatch.DrawString(spriteFont, "Burstfire: " + p.rounds[3], new Vector2(600, 110), Color.White);
+                    
+                }
+                j++;
+                i = 0;
+                rounds = p.rounds[3];
+                while (rounds > 25 && i < 4)
+                {
+                    spriteBatch.Draw(gun3, new Rectangle(614 + offsetX * i, 85 + offsetY * j, 15, 15), Color.White);
+                    i++;
+                    rounds -= 25;
+                    //spriteBatch.DrawString(spriteFont, "Automatic: " + p.rounds[4], new Vector2(600, 135), Color.White);
+              
+                }
+                j++;
+              
+               
             }
             if (ShowLevelClearedText)
                 spriteBatch.Draw(complete, rectFinish, Color.White);
